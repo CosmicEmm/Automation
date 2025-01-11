7c97ca276fdf4cd409e750393cbe66458cf56d2b
Muhammad <muhammadmalik5303@gmail.com>
2025-01-11 13:42:04 +0500

    Update footy.py

--- footy.py
+++ footy.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import lxml # not used directly, but pandas uses lxml internally for parsing
+from sys import argv
 
 
-scraper = pd.read_html('https://en.wikipedia.org/wiki/Jude_Bellingham')
+scraper = pd.read_html(f'https://en.wikipedia.org/wiki/{argv[1]}')
+print(argv)
 
-for index, table in enumerate(scraper): # Iterates over the DataFrames
-    print('********************************************')
-    print(index)
-    print(table)
+# for index, table in enumerate(scraper): # Iterates over the DataFrames
+#     print('********************************************')
+#     print(index)
+#     print(table)
 
 print(scraper[2]) # Prints the 3rd table (index 2) from the list of DataFrames
